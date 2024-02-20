8b1c587e4a9363a8f8092495eb130cba156db61c
tommyrharper <thomasrobertharper@gmail.com>
2024-02-20 15:32:54 +0000

    👷🏻 🧹 Add Uint256 folder

--- src/UnitTypes.sol
+++ src/UnitTypes.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.19;
 
-import {BaseUint256} from "./BaseUint256/ValueType.sol";
-import {BaseQuantoPerUSDUint256} from "./BaseQuantoPerUSDUint256/ValueType.sol";
-import {QuantoUint256} from "./QuantoUint256/ValueType.sol";
-import {USDPerBaseUint256} from "./USDPerBaseUint256/ValueType.sol";
-import {USDPerQuantoUint256} from "./USDPerQuantoUint256/ValueType.sol";
-import {USDUint256} from "./USDUint256/ValueType.sol";
+import {BaseUint256} from "./Uint256/BaseUint256/ValueType.sol";
+import {BaseQuantoPerUSDUint256} from "./Uint256/BaseQuantoPerUSDUint256/ValueType.sol";
+import {QuantoUint256} from "./Uint256/QuantoUint256/ValueType.sol";
+import {USDPerBaseUint256} from "./Uint256/USDPerBaseUint256/ValueType.sol";
+import {USDPerQuantoUint256} from "./Uint256/USDPerQuantoUint256/ValueType.sol";
+import {USDUint256} from "./Uint256/USDUint256/ValueType.sol";
