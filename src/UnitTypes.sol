a37e3bc85018abd6388f88cb7f38dd15a097e6b1
tommyrharper <thomasrobertharper@gmail.com>
2024-02-21 15:35:51 +0000

    👷🏻 Import Uint128s to UnitTypes.sol

--- src/UnitTypes.sol
+++ src/UnitTypes.sol
@@ -1,6 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.19;
 
+/*//////////////////////////////////////////////////////////////
+                            UINT128
+//////////////////////////////////////////////////////////////*/
+
+import {BaseUint128} from "./Uint128/BaseUint128/ValueType.sol";
+import {BaseQuantoPerUSDUint128} from "./Uint128/BaseQuantoPerUSDUint128/ValueType.sol";
+import {QuantoUint128} from "./Uint128/QuantoUint128/ValueType.sol";
+import {USDPerBaseUint128} from "./Uint128/USDPerBaseUint128/ValueType.sol";
+import {USDPerQuantoUint128} from "./Uint128/USDPerQuantoUint128/ValueType.sol";
+import {USDUint128} from "./Uint128/USDUint128/ValueType.sol";
+
+/*//////////////////////////////////////////////////////////////
+                            UINT256
+//////////////////////////////////////////////////////////////*/
+
 import {BaseUint256} from "./Uint256/BaseUint256/ValueType.sol";
 import {BaseQuantoPerUSDUint256} from "./Uint256/BaseQuantoPerUSDUint256/ValueType.sol";
 import {QuantoUint256} from "./Uint256/QuantoUint256/ValueType.sol";
