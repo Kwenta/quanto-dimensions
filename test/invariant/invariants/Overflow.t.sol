3ecf36e235ed56e563e15cb0cbf6c12451861c09
JaredBorders <jaredborders@pm.me>
2024-04-04 17:09:34 -0400

    ✅ move invariants to specific dir

--- test/invariant/invariants/Overflow.t.sol
+++ test/invariant/invariants/Overflow.t.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.8.19;
 
 import {Test} from "forge-std/Test.sol";
-import {Handler, BaseInt128Handler, BaseInt128} from "./handlers/Handler.sol";
+import {Handler, BaseInt128Handler, BaseInt128} from "../handlers/Handler.sol";
 
 contract OverflowInvariant is Test {
     BaseInt128Handler public baseInt128Handler;
@@ -12,7 +12,7 @@ contract OverflowInvariant is Test {
         handler = new Handler({_baseInt128Handler: baseInt128Handler});
 
         bytes4[] memory selectors = new bytes4[](1);
-        selectors[0] = Handler.ghost_add.selector;
+        selectors[0] = Handler.ghost_add_BaseInt128.selector;
         //selectors[n] = Handler.forcePush.selector;
 
         targetSelector(
@@ -25,5 +25,21 @@ contract OverflowInvariant is Test {
     function invariant_Int128_BaseInt128_Interactions() public {
         BaseInt128 result = baseInt128Handler.ghostAddResult();
         assertGe(result.unwrap(), 0);
+    }
+
+    function invariant_addition() public {
+        assert(false);
+    }
+
+    function invariant_subtraction() public {
+        assert(false);
+    }
+
+    function invariant_multiplication() public {
+        assert(false);
+    }
+
+    function invariant_division() public {
+        assert(false);
     }
 }
--- test/invariant/invariants/Underflow.t.sol
+++ test/invariant/invariants/Underflow.t.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.8.19;
 
 import {Test} from "forge-std/Test.sol";
-import {Handler, BaseInt128Handler, BaseInt128} from "./handlers/Handler.sol";
+import {Handler, BaseInt128Handler, BaseInt128} from "../handlers/Handler.sol";
 
 contract UnderflowInvariant is Test {
     BaseInt128Handler public baseInt128Handler;
@@ -11,23 +11,15 @@ contract UnderflowInvariant is Test {
         baseInt128Handler = new BaseInt128Handler();
         handler = new Handler({_baseInt128Handler: baseInt128Handler});
 
-        bytes4[] memory selectors = new bytes4[](1);
-        selectors[0] = Handler.ghost_add.selector;
-        //selectors[n] = Handler.forcePush.selector;
+        // bytes4[] memory selectors = new bytes4[](1);
+        // selectors[0] = Handler.ghost_add.selector;
+        // //selectors[n] = Handler.forcePush.selector;
 
-        targetSelector(
-            FuzzSelector({addr: address(handler), selectors: selectors})
-        );
+        // targetSelector(
+        //     FuzzSelector({addr: address(handler), selectors: selectors})
+        // );
 
         targetContract(address(handler));
-    }
-
-    /// @custom:todo 
-    /// if x is of type T and y is of type T
-    /// then the invariant is violated 
-    /// if y > 0 and x - y > x
-    function invariant_subtraction() public pure {
-        assert(false);
     }
 
     /// @custom:todo
@@ -35,6 +27,14 @@ contract UnderflowInvariant is Test {
     /// then the invariant is violated
     /// if x < 0 and y < 0 and x + y > x
     function invariant_addition() public pure {
+        assert(false);
+    }
+
+    /// @custom:todo
+    /// if x is of type T and y is of type T
+    /// then the invariant is violated
+    /// if y > 0 and x - y > x
+    function invariant_subtraction() public pure {
         assert(false);
     }
 
